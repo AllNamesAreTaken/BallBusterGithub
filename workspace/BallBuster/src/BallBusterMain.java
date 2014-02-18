972cf49dbd4ba1749dc65cd8ed55f1ba03afdc2e
Matt <mkp803@jagmail.southalabama.edu>
2014-02-18 13:11:41 -0600

    Added .project/.classpath and changes to main

--- workspace/BallBuster/src/BallBusterMain.java
+++ workspace/BallBuster/src/BallBusterMain.java
@@ -1,5 +1,6 @@
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
+import lejos.nxt.NXT;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
@@ -13,10 +14,10 @@ public class BallBusterMain {
 	private static UltrasonicSensor u1 = new UltrasonicSensor(SensorPort.S1);
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		buster.setSpeed(800);
+		
 		while(!Button.ESCAPE.isDown())
 		{
-			if(u1.getDistance() < 10)
+			if(u1.getDistance() < 15 && u1.getDistance() > 10)
 			{
 				hitBall();
 			}
@@ -28,10 +29,12 @@ public class BallBusterMain {
 		u1.setMode(u1.off());
 		while(buster.getPosition() < 90)
 		{
+			buster.setSpeed(800);
 			buster.forward();
 		}
 		while(buster.getPosition() != 0)
 		{
+			buster.setSpeed(100);
 			if(buster.getPosition() > 0)
 				buster.backward();
 			if(buster.getPosition() < 0)
