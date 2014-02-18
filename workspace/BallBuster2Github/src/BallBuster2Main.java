0d0091516f29e4d4f3c8d20f31ca0123de5489cf
Fluffikinz <wdh1102@jagmail.southalabama.edu>
2014-02-18 12:58:35 -0600

    Imported Logging Classes
    Adding for later and testing Github commit.

--- workspace/BallBuster2Github/src/BallBuster2Main.java
+++ workspace/BallBuster2Github/src/BallBuster2Main.java
@@ -3,6 +3,8 @@ import lejos.nxt.Motor;
 import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.comm.NXTConnection;
+import lejos.util.NXTDataLogger;
 
 
 
@@ -11,6 +13,8 @@ public class BallBuster2Main {
 
 	private static NXTRegulatedMotor buster = Motor.A;
 	private static UltrasonicSensor u1 = new UltrasonicSensor(SensorPort.S1);
+	private static NXTDataLogger log;
+	private static NXTConnection logConn;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		while(!Button.ESCAPE.isDown())
