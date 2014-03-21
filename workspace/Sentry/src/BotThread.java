debe428e033b16e19910b131245bfaf2b056fa7f
Matt <mkp803@jagmail.southalabama.edu>
2014-03-21 17:54:51 -0500

    added Navigator class to BotThread

--- workspace/Sentry/src/BotThread.java
+++ workspace/Sentry/src/BotThread.java
@@ -1,4 +1,8 @@
 import lejos.nxt.Button;
+import lejos.nxt.Motor;
+import lejos.robotics.navigation.DifferentialPilot;
+import lejos.robotics.navigation.MoveController;
+import lejos.robotics.navigation.Navigator;
 
 
 public class BotThread extends Thread {
@@ -6,6 +10,9 @@ public class BotThread extends Thread {
 	public void run()
 	{
 		int count = 0;
+		//MoveController MC = new MoveController(Motor.A, Motor.B);
+		DifferentialPilot pilot = new DifferentialPilot(5.6,14, Motor.A, Motor.C);
+		Navigator n = new Navigator(pilot);
 		while(!Button.ESCAPE.isDown())
 		{
 			//if(Button.LEFT.isDown())
@@ -22,5 +29,6 @@ public class BotThread extends Thread {
 			}
 		}
 		System.out.println("quitting");
+		Motor.A.stop();
 	}
 }
