8034b37f15956780e28708866f61bbaeeab5f7b1
Matt <mkp803@jagmail.southalabama.edu>
2014-03-21 17:36:14 -0500

    Threads are working

--- workspace/Sentry/src/BotThread.java
+++ workspace/Sentry/src/BotThread.java
@@ -5,12 +5,22 @@ public class BotThread extends Thread {
 
 	public void run()
 	{
+		int count = 0;
 		while(!Button.ESCAPE.isDown())
 		{
-			if(!Button.LEFT.isDown())
+			//if(Button.LEFT.isDown())
 			{
-			System.out.println("in BotThread");
+				if(!this.isInterrupted())
+				{
+					System.out.println(count + "\n");
+					count++;
+				}
+				else
+				{
+					break;
+				}
 			}
 		}
+		System.out.println("quitting");
 	}
 }
--- workspace/Sentry/src/Main.java
+++ workspace/Sentry/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			BotThread 
+			BotThread bt = new BotThread();
+			bt.start();
 	}
 
 }
