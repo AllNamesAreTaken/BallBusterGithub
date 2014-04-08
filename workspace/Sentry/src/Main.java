4627e224fff992e6ec7f1f947453ab091d18a87f
Matt <mkp803@jagmail.southalabama.edu>
2014-04-08 17:02:13 -0500

    added lib

--- workspace/Sentry/src/Main.java
+++ workspace/Sentry/src/Main.java
@@ -2,6 +2,17 @@ import lejos.nxt.Button;
 import lejos.nxt.Motor;
 import lejos.nxt.remote.RemoteMotor;
 
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+
 
 public class Main {
 
