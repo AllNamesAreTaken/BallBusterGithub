6c3fe0eee56efa1302b95a5babc8cec4c394f658
Fluffikinz <wdh1102@jagmail.southalabama.edu>
2014-04-16 00:32:59 -0500

    Successful multithreading but gross code.

--- workspace/Sentry/src/ImageDetection.java
+++ workspace/Sentry/src/ImageDetection.java
@@ -7,100 +7,107 @@ import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 
-
-public class ImageDetection implements Runnable {
+public class ImageDetection extends Thread {
 	private VideoCapture capture;
 	private Mat oriImg;
 	private Mat hsvImg;
 	private Mat redGsImg;
 	private Mat erode;
 	private Mat dilate;
-//	private Panel redGsPanel;
-//	private Frame redGsFrame;
-	
+	private double[] position;
+	private boolean canRun;
+	private Panel redGsPanel;
+	private Frame redGsFrame;
+
 	public ImageDetection() {
-		erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(3,3));
-		dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,5));
+	}
+
+	public void startDet() {
+		erode = Imgproc.getStructuringElement(Imgproc.MORPH_RECT,
+				new Size(3, 3));
+		dilate = Imgproc.getStructuringElement(Imgproc.MORPH_RECT, new Size(5,
+				5));
 		oriImg = new Mat();
 		hsvImg = new Mat();
 		redGsImg = new Mat();
-	}
-	
-	public void startCamera() {
+		position = new double[] { -1.0, -1.0 };
 		capture = new VideoCapture(1);
 		capture.set(3, 1000);
 		capture.set(4, 1000);
 
 		capture.read(oriImg);
-//		redGsPanel = new Panel();
-//		redGsFrame = new Frame(redGsPanel, "Grayscale", oriImg.width(), oriImg.height());
+		redGsPanel = new Panel();
+		redGsFrame = new Frame(redGsPanel, "Grayscale", oriImg.width(),
+				oriImg.height());
 		updateImage();
-	}
-	
-	public void stopCamera() {
-		capture.release();
-	}
-	
-	public double[] getRedBall() {
-		double[] bi = new double[2];
-		updateImage();
-
-		Scalar hsv_min = new Scalar(0, 70, 50, 0);
-		Scalar hsv_max = new Scalar(10, 255, 255, 0);
-		Core.inRange(hsvImg, hsv_min, hsv_max, redGsImg);
-		Imgproc.erode(redGsImg, redGsImg, erode);
-		Imgproc.dilate(redGsImg, redGsImg, dilate);
-
-        Mat circles = new Mat();
-		Imgproc.HoughCircles(redGsImg, circles, Imgproc.CV_HOUGH_GRADIENT, 2, 500, 40, 40, 20, 100);
-		if(circles.cols() > 0) 
-		{
-			bi[0] = circles.get(0, 0)[0];
-			bi[1] = circles.get(0, 0)[1];
-		}
-		else {
-			bi[0] = -1.0;
-			bi[1] = -1.0;
-		}
-		return bi;
-	}
-	
-	public double[] getGreenSquare() {
-		double[] bi = new double[2];
-		updateImage();
-
-		Scalar hsv_min = new Scalar(90, 70, 50, 0);
-		Scalar hsv_max = new Scalar(100, 255, 255, 0);
-		Core.inRange(hsvImg, hsv_min, hsv_max, redGsImg);
-		Imgproc.erode(redGsImg, redGsImg, erode);
-		Imgproc.dilate(redGsImg, redGsImg, dilate);
-
-        Mat circles = new Mat();
-		Imgproc.HoughCircles(redGsImg, circles, Imgproc.CV_HOUGH_GRADIENT, 2, 500, 40, 40, 20, 100);
-		if(circles.cols() > 0) 
-		{
-			bi[0] = circles.get(0, 0)[0];
-			bi[1] = circles.get(0, 0)[1];
-		}
-		else {
-			bi[0] = -1.0;
-			bi[1] = -1.0;
-		}
-		return bi;
-	}
-	
-	private void updateImage() {
-		capture.read(oriImg);
-
-//	    redGsPanel.setimagewithMat(oriImg);//redGsImg);
-//	    redGsFrame.repaint();
-		Imgproc.GaussianBlur(oriImg, oriImg, new Size(11,11), 30.0);
-		Imgproc.cvtColor(oriImg, hsvImg, Imgproc.COLOR_BGR2HSV);
+		canRun = true;
 	}
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
-		
+		while (canRun) {
+			double[] bi = new double[2];
+			updateImage();
+			Scalar hsv_min = new Scalar(0, 70, 50, 0);
+			Scalar hsv_max = new Scalar(10, 255, 255, 0);
+			Core.inRange(hsvImg, hsv_min, hsv_max, redGsImg);
+			Imgproc.erode(redGsImg, redGsImg, erode);
+			Imgproc.dilate(redGsImg, redGsImg, dilate);
+
+			Mat circles = new Mat();
+			Imgproc.HoughCircles(redGsImg, circles, Imgproc.CV_HOUGH_GRADIENT,
+					2, 500, 40, 40, 20, 100);
+			if (circles.cols() > 0) {
+				bi[0] = circles.get(0, 0)[0];
+				bi[1] = circles.get(0, 0)[1];
+			} else {
+				bi[0] = -1.0;
+				bi[1] = -1.0;
+			}
+			// Hopefully prevents race conditions
+			position = bi;
+		}
 	}
+
+	public void stopDet() {
+		canRun = false;
+		capture.release();
+	}
+
+	public double[] getRedBall() {
+		return position;
+	}
+
+	// public double[] getGreenSquare() {
+	// double[] bi = new double[2];
+	// updateImage();
+	//
+	// Scalar hsv_min = new Scalar(90, 70, 50, 0);
+	// Scalar hsv_max = new Scalar(100, 255, 255, 0);
+	// Core.inRange(hsvImg, hsv_min, hsv_max, redGsImg);
+	// Imgproc.erode(redGsImg, redGsImg, erode);
+	// Imgproc.dilate(redGsImg, redGsImg, dilate);
+	//
+	// Mat circles = new Mat();
+	// Imgproc.HoughCircles(redGsImg, circles, Imgproc.CV_HOUGH_GRADIENT, 2,
+	// 500, 40, 40, 20, 100);
+	// if (circles.cols() > 0) {
+	// bi[0] = circles.get(0, 0)[0];
+	// bi[1] = circles.get(0, 0)[1];
+	// } else {
+	// bi[0] = -1.0;
+	// bi[1] = -1.0;
+	// }
+	// return bi;
+	// }
+
+	private void updateImage() {
+		capture.read(oriImg);
+
+		redGsPanel.setimagewithMat(oriImg);// redGsImg);
+		redGsFrame.repaint();
+		Imgproc.GaussianBlur(oriImg, oriImg, new Size(11, 11), 30.0);
+		Imgproc.cvtColor(oriImg, hsvImg, Imgproc.COLOR_BGR2HSV);
+	}
+
 }
--- workspace/Sentry/src/Main.java
+++ workspace/Sentry/src/Main.java
@@ -1,4 +1,7 @@
 import java.io.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import lejos.nxt.Button;
 import lejos.nxt.Motor;
@@ -17,110 +20,57 @@ import org.opencv.highgui.Highgui;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 
-
 public class Main {
 
-	static Bot robot = new Bot(Motor.A, Motor.B);
-	
+//	static Bot robot = new Bot(Motor.A, Motor.B);
+	static ImageDetection imgd;
 
 	public static void main(String[] args) {
 		PrintWriter logger = null;
-		
-		// TODO Auto-generated method stub
+		imgd = new ImageDetection();
+
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-//		Don't uncomment if static class is
-//		Bot robot = new Bot(Motor.A, Motor.B);
-//		robot.resetRamp();
-		try
-		{
-			logger = new PrintWriter(new File("LogData.txt"));
-		}
-		catch(FileNotFoundException ex)
-		{
+
+		try {
+			logger = new PrintWriter(new BufferedWriter(new FileWriter("LogData.csv", true)));
+		} catch (IOException ex) {
 			System.out.println("Logging file not found");
 		}
-        
-		ImageDetection imgd = new ImageDetection();
-		imgd.startCamera();
-		
-		Motor.A.setSpeed(800);
-		Motor.B.setSpeed(50);
-		Motor.B.resetTachoCount();
+
+
+//		Motor.A.setSpeed(800);
+//		Motor.B.setSpeed(50);
+//		Motor.B.resetTachoCount();
 		Boolean quit = false;
 		long startTime = 0;
-		double endTime = 1;
-		while(!Button.ESCAPE.isDown())
-		{
+		long endTime = 1;
+		int count = 0;
+		DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd|HH:mm:ss");
+		//get current date time with Date()
+		Date date = new Date();
+		logger.append("\n<" + /*dateFormat.format(date)*/"DWG" + ">,");
+
+		imgd.startDet();
+		imgd.start();
+		while (!Button.ESCAPE.isDown()) {
 			startTime = System.currentTimeMillis();
 			double[] test = imgd.getRedBall();
 			System.out.println(test[0] + " " + test[1]);
-			if(test[0] > 0)
-			{
-				robot.hit(20);
+			if (test[0] > 0) {
+//				robot.hit(20);
 			}
 			endTime = System.currentTimeMillis();
-			if(logger != null)
-			{
-				logger.append(endTime - startTime + ",");
-			}
+			
+//			if (logger != null) {
+//				logger.append((endTime - startTime) + ",");
+//			}
+//			count++;
+//			System.out.println(count + " ");
 		}
-		
-//			Motor.B.forward();
-//			
-//			while(!Button.ENTER.isDown())
-//			{
-//				double[] test = imgd.getRedBall();
-//				if(Motor.B.getTachoCount() > 20)
-//				{
-//					Motor.B.backward();
-//				}
-//				if(Motor.B.getTachoCount() < -20)
-//				{
-//					Motor.B.forward();
-//				}
-//				if(Button.ESCAPE.isDown())
-//				{
-//					quit = true;
-//					break;
-//				}
-//			}
-//			if(quit)
-//			{
-//				break;
-//			}
-//			Motor.B.stop();
-//			long startTime = System.currentTimeMillis();
-//			Motor.A.rotate(360);
-//			long endTime = System.currentTimeMillis();
-//			System.out.println(endTime-startTime);
-////			break;
-//			try {
-//				Thread.sleep(8);
-//			} catch (Exception e) {
-//				// TODO: handle exception
-//			}
-//		}
-//		System.out.println("quitting");
-//		Motor.A.stop();
-//		Motor.A.flt();
-//		Motor.B.rotateTo(0);
-//		Motor.B.stop();
-//		Motor.B.flt();
-//		
-//			BotThread bt = new BotThread();
-////			Turner turn = new Turner();
-//			bt.start();
-////			turn.start();
-//			
-//			while(!Button.ESCAPE.isDown())
-//			{
-//				if(bt.isInterrupted())
-//					bt.run();
-////				if(turn.run();
-//			}
-			logger.close();
-			imgd.stopCamera();
-			System.exit(0);
+
+		logger.close();
+		imgd.stopDet();
+		System.exit(0);
 	}
 
 }
